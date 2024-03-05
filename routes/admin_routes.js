a05b4fa897e11bb6a299154e54e9f30c67eee3a1
MParry <meilir@live.co.uk>
2024-03-05 17:23:15 +0000

    add login page

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const path = require('path');
+
 router.get('/', (req, res) => {
     res.send('admin side');
 });
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve("src/admin/login.html"));
+});
+
 
 module.exports = router;
