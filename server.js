7a6cdd6144b0666b512db9e819d6ca226d1f7d0e
VirejDasani <dasanivirej@gmail.com>
2024-03-05 15:43:38 +0000

    Add index.html

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-const customerRouter = require('./routes/cust_routes');
-const adminRouter = require('./routes/admin_routes');
+const customerRouter = require("./routes/cust_routes");
+const adminRouter = require("./routes/admin_routes");
 
-app.get('/', (req, res) => {
-    res.send("test");
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-app.use('/customer', customerRouter);
+app.use("/customer", customerRouter);
 
-app.use('/admin', adminRouter);
+app.use("/admin", adminRouter);
 
 app.listen(port, () => {
-  console.log('Started server');
+  console.log("Started server");
 });
