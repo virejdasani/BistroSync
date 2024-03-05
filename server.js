3cb1e1be733cb2071c5cf05142288098518a15df
VirejDasani <dasanivirej@gmail.com>
2024-03-05 16:03:46 +0000

    setup subpaths

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const customerRouter = require("./routes/cust_routes");
+const restaurantRouter = require("./routes/restaurant_routes");
 const adminRouter = require("./routes/admin_routes");
 
-app.use(express.static('src'));
+app.use(express.static("src"));
 
 app.get("/", (req, res) => {
-  res.sendFile("src/index.html", { root: __dirname });
+  res.redirect("/restaurant");
 });
 
-app.use("/customer", customerRouter);
+app.get("/admin", (req, res) => {
+  res.sendFile("src/admin/index.html", { root: __dirname });
+});
+
+app.get("/restaurant", (req, res) => {
+  res.sendFile("src/restaurant/index.html", { root: __dirname });
+});
+
+app.use("/restaurant", restaurantRouter);
 
 app.use("/admin", adminRouter);
 
