ab4b96d1f93970614722ad7c71e440c4d7cb2bf5
MParry <meilir@live.co.uk>
2024-03-05 06:28:40 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const customerRouter = require('./routes/cust_routes');
+const adminRouter = require('./routes/admin_routes');
+
+app.get('/', (req, res) => {
+    res.send("test");
+});
+
+app.use('/customer', customerRouter);
+
+app.use('/admin', adminRouter);
+
+app.listen(port, () => {
+  console.log('Started server');
+});
