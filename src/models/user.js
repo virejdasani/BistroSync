24a64f22c580f124faa6d32e044669716d22d7dd
MParry <meilir@live.co.uk>
2024-03-13 10:20:28 +0000

    change user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
 
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Username cannot be blank']
