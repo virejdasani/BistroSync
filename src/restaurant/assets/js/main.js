df1cd37d2980b3b00dce2568ce7bfb61a5b08cd0
VirejDasani <dasanivirej@gmail.com>
2024-04-01 16:26:11 +0100

    mobile navbar works

--- src/restaurant/assets/js/main.js
+++ src/restaurant/assets/js/main.js
@@ -1,3 +1,4 @@
+// logic for mobile menu toggle
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
@@ -19,33 +20,3 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-const sections = document.querySelectorAll("section[id]");
-
-window.addEventListener("scroll", scrollActive);
-
-function scrollActive() {
-  const scrollY = window.pageYOffset;
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav_menu a[href*=" + sectionId + "]")
-        .classList.add("active");
-    } else {
-      document
-        .querySelector(".nav_menu a[href*=" + sectionId + "]")
-        .classList.remove("active");
-    }
-  });
-}
-
-window.onscroll = () => {
-  const nav = document.getElementById("header");
-  if (this.scrollY >= 200) nav.classList.add("scroll-header");
-  else nav.classList.remove("scroll-header");
-};
