7880b7ac5b32d3cfa29005a5f33e83e4390a74e8
VirejDasani <dasanivirej@gmail.com>
2024-04-01 16:30:24 +0100

    api testing

--- src/restaurant/assets/js/main.js
+++ src/restaurant/assets/js/main.js
@@ -1,3 +1,31 @@
+// fetch this API: https://raw.githubusercontent.com/virejdasani/BistroSync/main/api/menu.json?
+
+// fetch the menu items from the API
+fetch(
+  "https://raw.githubusercontent.com/virejdasani/BistroSync/main/api/menu.json"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const menu = data.menu;
+    const menuContainer = document.querySelector(".menu_container");
+
+    menu.forEach((item) => {
+      const menuItem = document.createElement("div");
+      menuItem.classList.add("menu_item");
+
+      menuItem.innerHTML = `
+      <img src="${item.img}" alt="${item.title}" class="menu_img">
+      <div class="menu_content">
+        <h3 class="menu_title">${item.title}</h3>
+        <span class="menu_price
+        ">${item.price}</span>
+        <p class="menu_description">${item.desc}</p>
+      </div>
+      `;
+      menuContainer.appendChild(menuItem);
+    });
+  });
+
 // logic for mobile menu toggle
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
